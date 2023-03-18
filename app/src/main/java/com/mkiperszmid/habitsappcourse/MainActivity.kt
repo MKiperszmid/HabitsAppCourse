03cb6d9afd1f32e49a9f14648d204ee254ace5f2
Martin Kiperszmid <MKiperszmid@gmail.com>
2023-03-18 17:18:45 -0300

    Initial commit

---
+++ app/src/main/java/com/mkiperszmid/habitsappcourse/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.mkiperszmid.habitsappcourse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.mkiperszmid.habitsappcourse.ui.theme.HabitsAppCourseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HabitsAppCourseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}

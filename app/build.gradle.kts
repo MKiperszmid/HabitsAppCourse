a8f668f73ad211b2489ab0c003f4472a843b2c53
Martin Kiperszmid <MKiperszmid@gmail.com>
2024-03-08 18:43:11 -0300

    Fix variable naming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,19 +60,19 @@ dependencies {
         }
     }
 
-    //Get day of week api 25 or lower
+    // Get day of week api 25 or lower
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 
     val timePicker = "1.1.0"
     implementation("com.maxkeppeler.sheets-compose-dialogs:core:$timePicker")
     implementation("com.maxkeppeler.sheets-compose-dialogs:clock:$timePicker")
 
-    val compose_version = "1.3.2"
+    val composeVersion = "1.3.2"
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.compose.ui:ui:$compose_version")
-    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.compose.material3:material3:1.1.0-alpha06")
 
     // Compose Navigation
@@ -85,9 +85,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:20.4.1")
 
     // Dagger Hilt
-    val hilt_version = "2.45"
-    implementation("com.google.dagger:hilt-android:$hilt_version")
-    kapt("com.google.dagger:hilt-compiler:$hilt_version")
+    val hiltVersion = "2.45"
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
 
@@ -95,28 +95,28 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.2.2")
 
     // Pager
-    val accompanist_version = "0.28.0"
-    implementation("com.google.accompanist:accompanist-pager:$accompanist_version")
-    implementation("com.google.accompanist:accompanist-pager-indicators:$accompanist_version")
+    val accompanistVersion = "0.28.0"
+    implementation("com.google.accompanist:accompanist-pager:$accompanistVersion")
+    implementation("com.google.accompanist:accompanist-pager-indicators:$accompanistVersion")
 
     // Permissions
-    implementation("com.google.accompanist:accompanist-permissions:$accompanist_version")
+    implementation("com.google.accompanist:accompanist-permissions:$accompanistVersion")
 
     // Room
-    val room_version = "2.5.0"
-    implementation("androidx.room:room-ktx:$room_version")
-    kapt("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-runtime:$room_version")
+    val roomVersion = "2.5.0"
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-runtime:$roomVersion")
 
     // Retrofit
-    val retrofit_version = "2.9.0"
-    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
-    implementation("com.squareup.retrofit2:converter-moshi:$retrofit_version")
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
 
     // WorkManager
-    val workmanager_version = "2.8.0"
-    implementation("androidx.work:work-runtime-ktx:$workmanager_version")
+    val workmanagerVersion = "2.8.0"
+    implementation("androidx.work:work-runtime-ktx:$workmanagerVersion")
     implementation("androidx.hilt:hilt-work:1.0.0")
 
     // Testing
@@ -124,13 +124,13 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_version")
-    kaptAndroidTest("com.google.dagger:hilt-android-compiler:$hilt_version")
-    debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:$compose_version")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    kaptAndroidTest("com.google.dagger:hilt-android-compiler:$hiltVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
     testImplementation("app.cash.turbine:turbine:0.7.0")
-    androidTestImplementation("com.google.dagger:hilt-android-testing:$hilt_version")
-    val mockk_version = "1.13.4"
-    testImplementation("io.mockk:mockk:$mockk_version")
-    androidTestImplementation("androidx.work:work-testing:$workmanager_version")
+    androidTestImplementation("com.google.dagger:hilt-android-testing:$hiltVersion")
+    val mockkVersion = "1.13.4"
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    androidTestImplementation("androidx.work:work-testing:$workmanagerVersion")
 }

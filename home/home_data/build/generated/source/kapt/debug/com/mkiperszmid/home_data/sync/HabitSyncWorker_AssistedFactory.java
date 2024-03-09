836a9933d1abbd132db847bee881e3029e4ebf76
Martin Kiperszmid <>
2024-03-09 16:51:49 -0300

    Remove modules

--- home/home_data/build/generated/source/kapt/debug/com/mkiperszmid/home_data/sync/HabitSyncWorker_AssistedFactory.java
+++
@@ -1,8 +0,0 @@
-package com.mkiperszmid.home_data.sync;
-
-import androidx.hilt.work.WorkerAssistedFactory;
-import dagger.assisted.AssistedFactory;
-
-@AssistedFactory
-public interface HabitSyncWorker_AssistedFactory extends WorkerAssistedFactory<HabitSyncWorker> {
-}
--- home/home_data/build/tmp/kapt3/stubs/debug/com/mkiperszmid/home_data/remote/dto/HabitResponseKt.java
+++
@@ -1,7 +0,0 @@
-package com.mkiperszmid.home_data.remote.dto;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 7, 1}, k = 2, d1 = {"\u0000\u0010\n\u0000\n\u0002\u0010$\n\u0002\u0010\u000e\n\u0002\u0018\u0002\n\u0000*\"\u0010\u0000\"\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u00012\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u0001\u00a8\u0006\u0004"}, d2 = {"HabitResponse", "", "", "Lcom/mkiperszmid/home_data/remote/dto/HabitDto;", "home_data_debug"})
-public final class HabitResponseKt {
-}
